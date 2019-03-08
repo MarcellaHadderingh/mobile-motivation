ab4e1460d40a812900fa8df77605c6319573d31b
marcella <marcellavanduyn@gmail.com>
2019-03-08 11:24:57 +0100

    Ready Mobile Friendly

--- index.php
+++ index.php
@@ -21,6 +21,101 @@
 
 <body>
 
-        <div class="container-fluid title">
-            <h1 style="font-size:10vw;">Marcella<br />Hadderingh</h1>
+    <div class="container-fluid title">
+        <h1 style="font-size:10vw;">Marcella<br />Hadderingh</h1>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 text">
+                <p>Mijn naam is Marcella Hadderingh onlangs heb ik mij laten omscholen tot Developer.
+                    <p>Altijd al ben ik enorm geïnteresseerd in gamen, gadgets, techniek en het
+                        aansluiten/uitvogelen
+                        ervan, hier kan ik dan ook uren zoet mee zijn.
+                        Ik vind het leuk om met mijn handen te werken en iets moois te creëeren.</p>
+                    <p>Vrienden en familieleden bellen mij altijd voor het aansluiten en installeren van allerlei
+                        apparatuur en software, dit vind ik zo leuk dat ik daarom graag nieuwe vaardigheden wil
+                        leren
+                        en
+                        kennis op wil doen om hier uiteindelijk mijn werk van te kunnen maken.
+                    </p>
+                    <p>Dus ben ik op zoek gegaan naar een opleiding/omscholing en ben ik terecht gekomen bij
+                        CodeGorilla
+                        voor de omscholing tot Junior Webdeveloper.
+                    </p>
+                    <p>Dit project is voor mij een uitkomst met het oog op mijn gezin op financieel vlak omdat ik
+                        redelijk
+                        snel de arbeidsmarkt weer op kan maar voornamelijk op persoonlijk vlak, want het zou mij
+                        enorm
+                        veel
+                        voldoening geven om een baan te hebben die ik leuk vind, die bij mij past en waarin ik
+                        verder
+                        kan
+                        groeien.</p>
+                    <p>Ik zit helemaal op mijn plek hier want wat ik nu doe en leer vind ik ontzettend leuk ook al
+                        ben
+                        ik
+                        pas net begonnen ik wil ontzettend graag alles op dit vlak leren en hier mijn werk van
+                        maken.
+                        Hierin zie ik mijn toekomst.</p>
+            </div>
         </div>
+    </div>
+    <img src="/image/macbook.jpg" class="img-fluid bgimage" style ="margin:100px 0;">
+
+    <div class="container-fluid">
+            <div class="row">
+                <div class="col-8 text" style="margin-top:50px;">
+                    <p>Ik ben een developer, programmeur, designer, mensen helper, creatieveling en harde
+                        werker, dus ben ik ervan overtuigd dat ik als persoon pas in het team. Bovendien ben ik
+                        leergierig, secuur, werk ik planmatig en ben ik bij uitstek een teamplayer.</p>
+                    <p> Maar ik ben nog lang niet uitgeleerd als junior web developer en kom naast mijn unieke
+                        combinatie van eigenschappen en achtergrond ook om te ontwikkelen en te leren. Ik heb door mijn
+                        traineeship bij CodeGorilla een echte passie en nieuwsgierigheid gekregen in het vak van
+                        programmeren en dat smaakt naar meer.</P>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12" style="margin:150px 0px;">
+                    <ul class="list-translated">
+                        <li class="big">Developer</li>
+                        <li class="big">Designer</li>
+                        <li class="big">Creative</li>
+                        <li class="big">Hard worker</li>
+                        <li class="big">Studious</li>
+                        <li class="big">Teamplayer</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col" style="margin:250px 0px;">
+                    <p class="intress">Heb ik jou <br>intresse gewekt?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container fluid bgimage2">
+            <div class="row">
+                <div class="col-8" style="margin:50px 50px 100px;">
+                    <h2 class="neon">Neem gerust contact met mij op!</h2>
+                    <ul style="margin-top:50px;">
+                        <li class="tekst">ROOMWEG 91-A</li>
+                        <li class="tekst">7523 BM</li>
+                        <li class="tekst">ENSCHEDE</li>
+                        <li class="tekst">0646944691</li>
+                        <li style="margin-top:5px;text-align: center;">
+                            <a href="mailto:marcellavanduyn@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-2x social"></i></a>
+                            <a href="https://github.com/MarcellaHadderingh" target="_blank"><i id="git" class="fa fa-github-square fa-2x social"></i></a>
+                            <a href="https://www.linkedin.com/in/marcella-hadderingh-013115176/" target="_blank"><i id="linked"
+                                    class="fa fa-linkedin-square fa-2x social"></i></a>
+                            <a href="http://marcellahadderingh.herokuapp.com" target="_blank"><i id="user" class="fa fa-user fa-2x social"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
